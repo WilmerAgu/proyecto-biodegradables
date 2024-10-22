eececd7960e0fa2b871cdc86e7e668476e5a3bb9
Wilmer Agudelo <wilmer.com.co@gmail.com>
2024-10-22 10:30:50 -0500

    creando contenido de pagina envases y toallitas

--- src/pages/products/Envases.jsx
+++ src/pages/products/Envases.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import './Products.css'
 
 const Envases = () => {
   return (
     <div>
-        <h2>Esta es la pagina de envases</h2>  
-
-
+      <section className='products-sesion'>
+        <h2>Envases Biodegradables: Cuidando Nuestro Planeta</h2>
+        <p>Los envases biodegradables son una excelente alternativa para reducir el impacto 
+          ambiental de los productos desechables. Fabricados a partir de materiales naturales, 
+          se descomponen de manera rápida y segura, minimizando la acumulación de residuos en 
+          vertederos y océanos. Al elegir envases biodegradables, no solo apoyas la sostenibilidad, 
+          sino que también contribuyes a la preservación de nuestro planeta para 
+          las futuras generaciones.
+        </p>
+        <div className='products-container'>
+          <div className='product'>
+            <img src="/public/img/envases1.jpeg" alt="envase de carton" />
+            <h3>Envases de cartón</h3>
+            <p>Los envases de cartón son biodegradables y se descomponen rápidamente 
+          en el medio ambiente. Además, su reciclaje contribuye a la economía circular 
+          y reduce la tala de árboles. ¡Prefiere el cartón y fomenta la sostenibilidad!. </p>
+          </div>
+          <div className='product'>
+            <img src="/public/img/envases2.jpeg" alt="envase de bambu" />
+            <h3>Envases de bambú estilo natural</h3>
+            <p>LLos envases de bambú son una opción ecológica y biodegradable que ofrece una 
+              alternativa sostenible a los plásticos tradicionales. Este material renovable es 
+              ligero, resistente y se descompone fácilmente en el medio ambiente. Al elegir envases 
+              de bambú, no solo proteges el planeta, sino que también aportas un toque de estilo 
+              natural a tu vida cotidiana.
+            </p>
+          </div>
+        </div>
+      </section>  
     </div>
   )
 }
--- src/pages/products/Toallitas.jsx
+++ src/pages/products/Toallitas.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import './Products.css'
 
 export const Toallitas = () => {
     return (
-        <div>
-            <h2>Esta es la pagina de toallitas</h2>  
+        <section className='products-sesion'>
+            <h2>Toallitas Biodegradables</h2>
+            <p>Son productos desechables diseñados para descomponerse de manera natural en 
+                el medio ambiente. Su importancia radica en que reducen la contaminación y el 
+                impacto ambiental de las toallitas convencionales, que pueden tardar años en 
+                desintegrarse. Al optar por toallitas biodegradables, contribuyes a un consumo 
+                más responsable y sostenible, ayudando a proteger nuestros ecosistemas.
+            </p>
+            <div className='products-container'>
+                <div className='product'>
+                    <img src="/public/img/toallitas2.jpeg" alt="toallitas biodegradables" />
+                    <h3>Toallitas Ecológicas</h3>
+                    <p>Las toallas ecológicas están fabricadas con materiales sostenibles y 
+                        biodegradables, ofreciendo una alternativa responsable a las toallas 
+                        desechables. Su uso contribuye a reducir la generación de residuos y 
+                        promueve un estilo de vida más amigable con el medio ambiente.
+                    </p>
+                </div>
+                <div className='product'>
+                    <img src="/public/img/toallitas3.jpeg" alt="bolsa de tela" />
+                    <h3>Toallitas de tela</h3>
+                    <p>Las toallitas de tela son una alternativa ecológica y reusable a las toallitas 
+                        desechables. Fabricadas con materiales suaves y duraderos, ayudan a reducir 
+                        el desperdicio y son perfectas para diversas aplicaciones, desde la limpieza 
+                        hasta el cuidado personal.  
+                    </p>
+                </div>
+                
+            </div>
 
-        </div>
+        </section>
     )
 }
